e73d605a83a02b9ff2fe00cee4999535360cce06
Brandon Kindred <brandon.kindred@gmail.com>
2015-12-21 19:24:32 -0500

    added preview and upload of user avatar

--- app/javascript/user/userServices.js
+++ app/javascript/user/userServices.js
@@ -2,7 +2,7 @@ var services = angular.module('buzzbands.UserServices', ['ngResource', 'buzzband
 
 services.factory('User', ['$resource', 'buzzbands.serviceConfig', function ($resource, config) {
     return $resource(config.basePath +'/admin/users/:id.json', {id: '@id'}, {
-        save: { method: 'PUT'},
+        update: { method: 'PUT'},
     });
   }
 ]);
