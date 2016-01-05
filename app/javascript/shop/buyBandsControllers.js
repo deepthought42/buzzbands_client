bfb19aaa113fda1d5a88caa134001984807e8bbd
Brandon Kindred <brandon.kindred@gmail.com>
2016-01-04 21:43:01 -0500

    Altered buy bands controller and view so that a package is chosen by default

--- app/javascript/shop/buyBandsControllers.js
+++ app/javascript/shop/buyBandsControllers.js
@@ -53,24 +53,25 @@ angular.module('buzzbands.BandsControllers', ['ui.router', 'buzzbands.BandsServi
 
    var getPackages = function(){
      return [{
-       count: '250',
+       count: 250,
        price: '25.00'
      },{
-       count: '500',
+       count: 500,
        price: '50.00'
      },{
-       count: '750',
+       count: 750,
        price: '75.00'
      },{
-       count: '1000',
+       count: 1000,
        price: '100.00'
      },{
-       count: '10000',
+       count: 10000,
        price: '1000.00'
      }]
    };
 
    $scope.bandPackages = getPackages();
    $scope.bandColors = getBandColors();
-
+   $scope.shopBands = {};
+   $scope.shopBands.package = $scope.bandPackages[0];
 }]);
