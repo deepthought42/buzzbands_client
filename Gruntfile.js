f54dd39c227f49b56a93545a0682dab04ff7f005
Brandon Kindred <brandon.kindred@gmail.com>
2016-07-22 20:25:16 -0400

    still working at it

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,7 +16,6 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('default', ['jshint']);
 
