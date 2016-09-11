731ee29168ea698308e1a4b84b0fcbdd8232f5f7
Brandon Kindred <brandon.kindred@gmail.com>
2016-09-11 00:37:08 -0400

    added in venue fields

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,23 +17,42 @@ module.exports = function(grunt) {
           build: ['Gruntfile.js', 'a/**/*.js']
         },
 
-        // configure uglify to minify js files -------------------------------------
-    uglify: {
-      options: {
-        banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
-      },
-      build: {
-        files: {
-          'dist/js/hypedrive.min.js': 'app/javascript/hypedrive.js'
+      // configure uglify to minify js files -------------------------------------
+      uglify: {
+        js: { //target
+            src: ['./public/min/app.js'],
+            dest: './public/min/app.js'
         }
+      },
+
+    ngAnnotate: {
+      options: {
+        singleQuotes: true
+      },
+      app: {
+        files: {
+          './public/min-safe/js/appFactory.js': ['./public/js/appFactory.js'],
+          './public/min-safe/js/FormController.js': ['./public/js/FormController.js'],
+          './public/min-safe/app.js': ['./public/js/app.js']
+        }
+      }
+    },
+
+    concat: {
+      js: { //target
+        src: ['./public/min-safe/app.js', './public/min-safe/js/*.js'],
+        dest: './public/min/app.js'
       }
     }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.registerTask('default', ['jshint', 'uglify']);
+  grunt.loadNpmTasks('grunt-ng-annotate');
+  grunt.loadNpmTasks('grunt-contrib-concat')
+  grunt.registerTask('default', ['ngAnnotate', 'concat', 'uglify']);
 
 };
